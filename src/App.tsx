9ae2a3e06a447184df258de1c4fbaf5bbdff0a38
Alexander <SaSkrkr123.com@mail.ru>
2025-07-10 14:27:08 +0300

    added animation text-main

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-
+import './animation.css'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <header>
         <ul>
           <li>JustTasks</li>
         </ul>
       </header>
+      
       <main>
+
         <div className='text-main'>
           <h2>Создай свой список с нами прям щас <img src= '/list.png' /> </h2> 
         </div>
+
+        <div>
+          
+        </div>
+
       </main>
     </div>
   );
