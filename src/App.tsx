17ca2a440d238bc9bc0388ecfbfadc849747137e
Alexander <SaSkrkr123.com@mail.ru>
2025-07-10 13:56:39 +0300

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <ul>
+          <li>JustTasks</li>
+        </ul>
+      </header>
     </div>
   );
 }
