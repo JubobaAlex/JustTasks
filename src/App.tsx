0bc80cf9b666d895cc41bcf5cd4b6117a40a3ccb
Alexander <SaSkrkr123.com@mail.ru>
2025-07-10 21:08:59 +0300

    added style

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import './animation.css'
 import './App.css';
 
 function App() {
-
+  
 const [inputValue, setInputValue] = useState('');
   class DataReceiver {
     protected content : HTMLElement | null = null
     getValues() {
       this.content = document.createElement('p')
       this.content.textContent = inputValue
+      this.content.classList.add('text-content')
        return this
     }
   }
@@ -17,19 +18,18 @@ const [inputValue, setInputValue] = useState('');
     
     showResults() {
        if (!this.content) return;
-      const element_div = document.querySelector('.cont') as HTMLDivElement || null
+
+      const element_div = document.querySelector('.content') as HTMLDivElement || null
       if(element_div) {
-        element_div.prepend(this.content)
+          element_div.prepend(this.content)
       }
       setInputValue('');
     }
   }
 
   function HandleClick() {
-  
     const data_printer = new DataPrinter()
-    
-    
+
     data_printer.getValues().showResults()
   }
 
@@ -52,10 +52,14 @@ const [inputValue, setInputValue] = useState('');
           <div className='input-wrapper'>
             <input onChange={(e) => setInputValue(e.target.value)} className='input' type='text' />
           </div>
+
+          <div className='div-btn'>
           <button onClick={HandleClick} className='save-btn'>Сохранить</button>
+          </div>
+
         </div>
-        
-        <div className='cont'>
+        <h2 className='spisok'>Ваш список:</h2>
+        <div className='content'>
         
         </div>
 
